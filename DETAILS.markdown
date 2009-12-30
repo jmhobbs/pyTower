0809617d1e01d09d7f99f50bef37ac4be5b4d235
John Hobbs <john@velvetcache.org>
2009-12-30 17:50:29 -0600

    Fleshed out the details. Added onto the tester, have a scrolling 'sky box'.

---
+++ DETAILS.markdown
@@ -0,0 +1,80 @@
+# Dimensions #
+## Floor Height ##
+In keeping with the original, a single floor is 35px high total. This breaks down to 10px of "flooring" and 25px of sprite area.
+## Floor Width ##
+All objects must be in increments of 10px wide. A single hotel room is 30px wide.
+## Playable Area ##
+Playable area is 3000x5250.  This is 150 stories, and 100 single hotel rooms wide.
+
+# Objects #
+Objects are flexible and described in YAML, classed as follows:
+
+* Lobby
+* Office
+* Housing
+* Hotel
+* Restaurant
+* Store
+* Entertainment
+* Services
+* Transportation
+
+## Attributes ##
+* Class
+* Name
+* Author
+* Sprites
+* Phases?
+* Rent
+
+## Scripting ##
+Should we allow custom scripting? How much?
+
+## Some Object Ideas ##
+* Lobby
+	* Floor lobby
+	* Sky lobby
+	* VIP Lobby?
+* Office
+	* Standard office
+	* Call center
+* Housing
+	* Condo
+* Hotel
+	* Single room
+	* Double room
+	* Suite
+	* Honeymoon suite
+* Restaurant
+	* Fast food
+	** Burgers
+	* Fine dining
+* Store
+	* Grocery Store
+	* Pet shop
+* Entertainment
+	* Movie theater
+* Services
+	* Trash
+	* Medical
+	* Parking
+* Transportation
+	* Stairs
+	* Elevator
+	* Escalator
+	* Express Elevator
+
+# People #
+People are typed as well. They are randomly generated and have traits.
+
+* Office worker
+* Family member (for housing)
+* Visitor (for shops, entertainment, hotel, food)
+
+# Time Scale #
+We will work monthly as in Yoot tower, instead of quarters.
+
+* 12 Months, 5 weekdays, 1 weekend in each.
+* Each spin of the loop is a 5 minute block == 288 frames a "day"
+* You can speed it up/slow it down as much as you want.
+* By default each frame is 0.10 long
--- test.py
+++ test.py
@@ -2,36 +2,61 @@
 import pygame, sys
 from pygame.locals import *
 
-# set up pygame
 pygame.init()
 
-# set up the window
-windowSurface = pygame.display.set_mode( ( 500, 400 ), 0, 32 )
-pygame.display.set_caption('Hello world!')
-
+# Dimensions
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+GAME_WIDTH = 3000 # Up to 100 single hotel rooms wide
+GAME_HEIGHT = 5250 # 150 Floors @ 35px each
+BOTTOM = GAME_HEIGHT - WINDOW_HEIGHT
+CENTER = ( GAME_WIDTH - WINDOW_WIDTH ) / 2
 # set up the colors
 BLACK = ( 0, 0, 0 )
 WHITE = ( 255, 255, 255 )
-LTBRN = ( 143, 106, 50 )
-DKBRN = ( 87, 63, 30 )
-SKYBLU = ( 128, 181, 255 )
-NIGHTSKYBLUE = ( 0, 49, 110 )
+LIGHT_BROWN = ( 143, 106, 50 )
+DARK_BROWN = ( 87, 63, 30 )
+SKY_BLUE = ( 128, 181, 255 )
+NIGHT_SKY_BLUE = ( 0, 49, 110 )
 
-# draw the white background onto the surface
-windowSurface.fill( SKYBLU )
+# set up the window
+windowSurface = pygame.display.set_mode( ( WINDOW_WIDTH, WINDOW_HEIGHT ), 0, 32 )
+pygame.display.set_caption( 'pyTower Testing Ground' )
 
-# draw the dirt onto the surface
-drawrect = ( windowSurface.get_rect().left, windowSurface.get_rect().bottom - 100, windowSurface.get_rect().width, 100  )
-pygame.draw.rect( windowSurface, LTBRN, drawrect )
-drawrect = ( windowSurface.get_rect().left, windowSurface.get_rect().bottom - 50, windowSurface.get_rect().width, 50  )
-pygame.draw.rect( windowSurface, DKBRN, drawrect )
+h_offset = CENTER
+v_offset = BOTTOM - 175 # We don't want absolute bottom, this is 5 floors up.
 
-# draw the window onto the screen
+fullSurface = pygame.Surface( ( GAME_WIDTH, GAME_HEIGHT ) )
+fullSurface.fill( SKY_BLUE )
+
+drawrect = ( fullSurface.get_rect().left, fullSurface.get_rect().bottom - 350, fullSurface.get_rect().width, 175 )
+pygame.draw.rect( fullSurface, LIGHT_BROWN, drawrect )
+drawrect = ( fullSurface.get_rect().left, fullSurface.get_rect().bottom - 175, fullSurface.get_rect().width, 175 )
+pygame.draw.rect( fullSurface, DARK_BROWN, drawrect )
+
+windowSurface.blit( fullSurface, ( 0, 0 ), ( 0 + h_offset, 0 + v_offset, WINDOW_WIDTH, WINDOW_HEIGHT ) )
 pygame.display.update()
 
-# run the game loop
 while True:
-		for event in pygame.event.get():
-				if event.type == QUIT:
-						pygame.quit()
-						sys.exit()
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+		elif event.type == KEYUP:
+			print "Keypress:", event.key
+			if event.key == 113: # q
+				pygame.quit()
+				sys.exit()
+			elif event.key == 274: # Down
+				v_offset = v_offset + 35
+			elif event.key == 273: # Up
+				v_offset = v_offset - 35
+			elif event.key == 276: # Right
+				h_offset = h_offset + 35
+			elif event.key == 275: # Left
+				h_offset = h_offset - 35
+			elif event.key == 278: # Home
+				h_offset = CENTER
+				v_offset = BOTTOM
+	windowSurface.blit( fullSurface, ( 0, 0 ), ( 0 + h_offset, 0 + v_offset, WINDOW_WIDTH, WINDOW_HEIGHT ) )
+	pygame.display.update()
