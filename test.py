0cdb544b2b2859ebd909c78c902a40c2643ad27c
John Hobbs <john@velvetcache.org>
2009-12-30 20:02:41 -0600

    Added a test mini-window. Need to change the math on some of this stuff.

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
 import pygame, sys
 from pygame.locals import *
 
@@ -7,10 +9,13 @@ pygame.init()
 # Dimensions
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
-GAME_WIDTH = 3000 # Up to 100 single hotel rooms wide
+GAME_WIDTH = 2975 # This is adjusted so the mini-window works.
 GAME_HEIGHT = 5250 # 150 Floors @ 35px each
 BOTTOM = GAME_HEIGHT - WINDOW_HEIGHT
 CENTER = ( GAME_WIDTH - WINDOW_WIDTH ) / 2
+MINI_WIDTH = 85
+MINI_HEIGHT = 150
+MINI_RATIO = 35
 # set up the colors
 BLACK = ( 0, 0, 0 )
 WHITE = ( 255, 255, 255 )
@@ -34,7 +39,16 @@ pygame.draw.rect( fullSurface, LIGHT_BROWN, drawrect )
 drawrect = ( fullSurface.get_rect().left, fullSurface.get_rect().bottom - 175, fullSurface.get_rect().width, 175 )
 pygame.draw.rect( fullSurface, DARK_BROWN, drawrect )
 
+miniSurface = pygame.Surface( ( MINI_WIDTH, MINI_HEIGHT ) )
+miniSurface.fill( SKY_BLUE )
+drawrect = ( miniSurface.get_rect().left, miniSurface.get_rect().bottom - ( 350 / MINI_RATIO ), miniSurface.get_rect().width, ( 175 / MINI_RATIO ) )
+pygame.draw.rect( miniSurface, LIGHT_BROWN, drawrect )
+drawrect = ( miniSurface.get_rect().left, miniSurface.get_rect().bottom - ( 175 / MINI_RATIO ), miniSurface.get_rect().width, ( 175 / MINI_RATIO ) )
+pygame.draw.rect( miniSurface, DARK_BROWN, drawrect )
+
+
 windowSurface.blit( fullSurface, ( 0, 0 ), ( 0 + h_offset, 0 + v_offset, WINDOW_WIDTH, WINDOW_HEIGHT ) )
+
 pygame.display.update()
 
 while True:
@@ -44,19 +58,30 @@ while True:
 			sys.exit()
 		elif event.type == KEYUP:
 			print "Keypress:", event.key
+			print "V:", v_offset
+			print "H:", h_offset
 			if event.key == 113: # q
 				pygame.quit()
 				sys.exit()
-			elif event.key == 274: # Down
-				v_offset = v_offset + 35
-			elif event.key == 273: # Up
+			elif event.key == 274 and v_offset + WINDOW_HEIGHT < GAME_HEIGHT: # Down
+					v_offset = v_offset + 35
+			elif event.key == 273 and v_offset > 0: # Up
 				v_offset = v_offset - 35
-			elif event.key == 276: # Right
-				h_offset = h_offset + 35
-			elif event.key == 275: # Left
+			elif event.key == 276 and h_offset + WINDOW_WIDTH < GAME_WIDTH: # Right
 				h_offset = h_offset - 35
+			elif event.key == 275 and h_offset > 0: # Left
+				h_offset = h_offset + 35
 			elif event.key == 278: # Home
 				h_offset = CENTER
 				v_offset = BOTTOM
 	windowSurface.blit( fullSurface, ( 0, 0 ), ( 0 + h_offset, 0 + v_offset, WINDOW_WIDTH, WINDOW_HEIGHT ) )
+
+	drawrect = ( 9, 9, MINI_WIDTH + 2, MINI_HEIGHT + 2 )
+	pygame.draw.rect( windowSurface, BLACK, drawrect )
+
+	windowSurface.blit( miniSurface, ( 10, 10 ), ( 0, 0, MINI_WIDTH, MINI_HEIGHT ) )
+
+	drawrect = ( 10 + ( h_offset / MINI_RATIO ), 10 + ( v_offset / MINI_RATIO ), ( WINDOW_WIDTH / MINI_RATIO ), ( WINDOW_HEIGHT / MINI_RATIO ) )
+	pygame.draw.rect( windowSurface, BLACK, drawrect )
+
 	pygame.display.update()
