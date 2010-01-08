f0849f393d60272eaabd72aa31f6ada97bab6c8c
John Hobbs <john@velvetcache.org>
2010-01-08 17:17:24 -0600

    Map tiles are now going to be jpegs, and saved by default at 85%.

--- maps/map_maker.sh
+++ maps/map_maker.sh
@@ -2,6 +2,7 @@
 
 if [ 4 -ne ${#@} ]; then
 	echo "Usage: $0 [file_to_split] [floors] [width_in_px] [output_dir]"
+	echo "Example: $0 default/day.jpg 110 2000 default/day"
 	exit
 fi
 
@@ -10,6 +11,6 @@ if [ ! -d $4 ]; then
 fi
 
 for i in $( seq 0 $(($2 - 1)) ); do
-	echo "convert $1 -crop $3x30+0+$(($i * 30)) $4/$(($2 - $i)).png"
-	convert $1 -crop $3x30+0+$(($i * 30)) $4/$(($2 - $i)).png
+	echo "convert $1 -quality 85 -crop $3x30+0+$(($i * 30)) $4/$(($2 - $i)).jpg"
+	convert $1 -quality 85 -crop $3x30+0+$(($i * 30)) $4/$(($2 - $i)).jpg
 done
